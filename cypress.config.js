90f649e1ce062abe94f49b63156d154e2cebc465
nikhilpurwant8791 <purwantn@gmail.com>
2024-05-30 00:54:38 +0530

    Initial Commit| Projet Setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
