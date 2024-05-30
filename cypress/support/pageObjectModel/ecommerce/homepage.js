f477d17dea917d463ae456342ba834fd96ef72e3
nikhilpurwant8791 <purwantn@gmail.com>
2024-05-30 17:19:28 +0530

    e-Com-02| Homepage Validation added

--- cypress/support/pageObjectModel/ecommerce/homepage.js
+++ cypress/support/pageObjectModel/ecommerce/homepage.js
@@ -1,14 +1,33 @@
 import loginData from '../../../fixtures/ecommerceLogindata.json';
+import list from '../../../fixtures/list.json'
 
-export default class HomePage{
-    constructor (){
+export default class HomePage {
+    constructor() {
         this.ele = {
-
+            list: '.list-group #itemc',
+            title: '.col-lg-9 #tbodyid .col-lg-4 .card-title',
+            description: '.col-lg-9 #tbodyid .col-lg-4 #article',
         }
     }
 
-    getLoginData(){
+    getTitleofElement() {
+        return cy.get(this.ele.title);
+    }
+
+    getElementDescription() {
+        return cy.get(this.ele.description);
+    }
+
+    getlistofCategories() {
+        return cy.get(this.ele.list);
+    }
+
+    getLoginData() {
         return loginData['testAccounts'];
+    }
+
+    getListOfElement() {
+        return list['Items'];
     }
 }
 
